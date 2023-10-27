66cc47f4abe4c4d92ad4569b76f702eb0eea1bb8
SteveWW <s@steveww.org>
2023-10-27 15:45:52 +0100

    complete endpoints

--- main.go
+++ main.go
@@ -19,6 +19,86 @@ func handleHealth(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "OK")
 }
 
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Key Value Store v1.0.0")
+}
+
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "PUT" {
+		vars := mux.Vars(r)
+		key := vars["key"]
+		val := vars["value"]
+
+		ctx := context.Background()
+		_, err := rdb.Set(ctx, key, val, 0).Result()
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error %v\n", err)
+		}
+
+		fmt.Fprintln(w, "OK")
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Wrong method")
+	}
+}
+
+func handleList(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		ctx := context.Background()
+		keys, err := rdb.Keys(ctx, "*").Result()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error %v\n", err)
+		} else {
+			for _, key := range keys {
+				fmt.Fprintln(w, key)
+			}
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Wrong method")
+	}
+}
+
+func handleFetch(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		vars := mux.Vars(r)
+		key := vars["key"]
+
+		ctx := context.Background()
+		val, err := rdb.Get(ctx, key).Result()
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintln(w, "Not found")
+		} else {
+			fmt.Fprintln(w, val)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Wrong method")
+	}
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "DEL" {
+		vars := mux.Vars(r)
+		key := vars["key"]
+
+		ctx := context.Background()
+		_, err := rdb.Del(ctx, key).Result()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error %v\n", err)
+		} else {
+			fmt.Fprintln(w, "OK")
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Wrong method")
+	}
+}
+
 func redisConnect() {
 	for {
 		log.Println("Connection to Redis")
@@ -34,6 +114,7 @@ func redisConnect() {
 			log.Printf("Not connected %v\n", err)
 			time.Sleep(5 * time.Second)
 		} else {
+			log.Println("Connected to Redis")
 			break
 		}
 	}
@@ -44,7 +125,12 @@ func main() {
 	redisConnect()
 
 	r := mux.NewRouter()
+	r.HandleFunc("/", handleHome)
 	r.HandleFunc("/health", handleHealth)
+	r.HandleFunc("/add/{key}/{value}", handleAdd)
+	r.HandleFunc("/fetch/{key}", handleFetch)
+	r.HandleFunc("/list", handleList)
+	r.HandleFunc("/del/{key}", handleDelete)
 	http.Handle("/", r)
 	err := http.ListenAndServe(":8080", r)
 	log.Fatal(err)
