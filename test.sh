3a89d256ba6fe34b6d8c76095d40f8307589fc1d
SteveWW <s@steveww.org>
2023-10-27 15:45:31 +0100

    initial load

---
+++ test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+URL="http://localhost:8080"
+KEYS="fee fi fo fum"
+DATA="tracethisdata"
+
+clear
+echo "Adding keys"
+for K in $KEYS
+do
+    echo "Adding $K"
+    curl -X PUT "${URL}/add/${K}/${DATA}${K}"
+done
+sleep 2
+
+echo ""
+echo "List keys"
+curl "${URL}/list"
+sleep 2
+
+echo ""
+echo "Getting keys"
+for K in $KEYS
+do
+    VAL=$(curl -s "${URL}/fetch/${K}")
+    echo "$K = $VAL"
+done
+sleep 2
+
+echo ""
+echo "Deleting keys"
+for K in $KEYS
+do
+    /bin/echo -n "$K "
+    curl -X DEL "${URL}/del/${K}"
+done
