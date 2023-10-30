0133b16e4cdd0cb9eb824e87567650d744bccf55
SteveWW <s@steveww.org>
2023-10-30 14:49:09 +0000

    more tests

--- test.sh
+++ test.sh
@@ -6,25 +6,30 @@ DATA="tracethisdata"
 
 clear
 echo "Home"
-curl "${URL}/"
+echo "===="
+curl -w '%{http_code}\n' "${URL}/"
 
 echo "Adding keys"
+echo "==========="
 for K in $KEYS
 do
     echo "Adding $K"
-    curl -X PUT "${URL}/add/${K}/${DATA}${K}"
+    curl -w '%{http_code}\n' -X PUT "${URL}/add/${K}/${DATA}-${K}"
 done
 sleep 2
 
 echo ""
 echo "List keys"
-curl "${URL}/list"
+echo "========="
+curl -w '%{http_code}\n' "${URL}/list"
 sleep 2
 
 echo ""
 echo "Getting keys"
+echo "============"
 for K in $KEYS
 do
+    curl -w '%{http_code}\n' "${URL}/fetch/${K}"
     VAL=$(curl -s "${URL}/fetch/${K}")
     echo "$K = $VAL"
 done
@@ -32,8 +37,14 @@ sleep 2
 
 echo ""
 echo "Deleting keys"
+echo "============="
 for K in $KEYS
 do
     /bin/echo -n "$K "
-    curl -X DEL "${URL}/del/${K}"
+    curl -w '%{http_code}\n' -X DEL "${URL}/del/${K}"
 done
+
+echo ""
+echo "Error"
+echo "====="
+curl -w '%{http_code}\n' "${URL}/add/ping/pong"
